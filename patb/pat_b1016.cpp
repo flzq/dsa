398bbe97c418ce38d9e8fef472bc9764ba5628c9
flzq <freedomcoder@163.com>
2020-05-18 21:09:01 +0800

    pat_b1016更改

---
+++ patb/pat_b1016.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+
+// 用于求Pa部分
+long long pa(long long& a, int da) {
+	int b;
+	long long c{ 0 };
+	// 循环计算Pa
+	while (a) {
+		b = a % 10;
+		a = a / 10;
+		if (b == da) {
+			c = c * 10 + b;
+		}
+	}
+	return c;
+}
+
+void pat_b1016() {
+	long long a, b, c;
+	int da, db;
+	scanf("%lld%d%lld%d", &a, &da, &b, &db);
+	printf("%lld", pa(a, da) + pa(b, db));
+}
