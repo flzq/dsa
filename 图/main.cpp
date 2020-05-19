f749bd041813eaa57748890e15f4aebde77c6151
freetime <644628521@qq.com>
2020-05-19 16:58:57 +0800

    实现MGraph删除边函数

--- 图/main.cpp
+++ 图/main.cpp
@@ -7,14 +7,19 @@ using namespace std;
 
 void test_graph() {
 	vector<string> v{ "AA", "CC", "BB" };
-	LGraph g2(v);
+	MGraph g2(v);
 
-	g2.print();
-	std::cout << std::endl;
+
 	g2.insert_edge("AA", "CC");
 	g2.insert_edge("CC", "BB", 66);
 	g2.insert_edge("XX", "OO");
 	g2.insert_edge(0, 3, 88);;
+	g2.print();
+	g2.remove_edge("AA", "CC");
+	cout << endl;
+	g2.print();
+	g2.remove_edge(1, 2);
+	cout << endl;
 	g2.print();
 
 	/*LGraph lg(v);
