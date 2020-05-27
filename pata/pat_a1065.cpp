e8619b3ee954e665e7d4c99d590addad2b366310
flzq <freedomcoder@163.com>
2020-05-27 15:05:06 +0800

    pat_a1065

---
+++ pata/pat_a1065.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+
+int pat_a1065() {
+	/*
+		数据中没有A或B取到2^63的情况
+	*/
+	int T;
+	long long a, b, c;
+	scanf("%d", &T);
+	for (int i = 1; i <= T; ++i) {
+		scanf("%lld%lld%lld", &a, &b, &c);
+		long long tmp = a + b;
+		if (a > 0 && b > 0 && tmp < 0) { // 处理正溢出:[-2, 2^63]
+			printf("Case #%d: true\n", i);
+		}
+		else if (a < 0 && b < 0 && tmp >= 0) { // 处理负溢出:[0, 2^63-1]
+			printf("Case #%d: false\n", i);
+		}
+		else {
+			if (tmp > c) {
+				printf("Case #%d: true\n", i);
+			}
+			else {
+				printf("Case #%d: false\n", i);
+			}
+		}
+	}
+}
