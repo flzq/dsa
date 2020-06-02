628d2df7fde10270d35c2e84c45e82a1ac2bdf7c
flzq <freedomcoder@163.com>
2020-06-02 19:00:44 +0800

    pat_b1009

--- patb/pat_b1009.cpp
+++ patb/pat_b1009.cpp
@@ -1,13 +1,18 @@
 #include <cstdio>
-
+char words[90][90];
 void pat_b1009() {
-	char words[80][81];
 	int cnt{ 0 };
-	while (scanf("%s", words[cnt++]) != EOF) {
-		printf("word: %s\n", words[cnt]);
+	while (scanf("%s", words[cnt]) != EOF) {
+		++cnt; // 注意cnt一定要在函数体中加一,否则会导致多一
+	}
+	for (int i = cnt - 1; i >= 0; --i) {
+		printf("%s", words[i]);
+		if (i != 0) {
+			printf(" ");
+		}
 	}
 }
 
-int main() {
-	pat_b1009();
-}
+//int main() {
+//	pat_b1009();
+//}
