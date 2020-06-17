2841988040d53a475b0bd5372fb1b7aa3a23f21d
flzq <freedomcoder@163.com>
2020-06-17 09:51:09 +0800

    pat_a1038； sort函数处理字符串数组比较麻烦，使用string进行处理

--- pata/pat_a1038.cpp
+++ pata/pat_a1038.cpp
@@ -1,52 +1,53 @@
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
-#include <cstring>
+#include <string>
 using std::sort;
+using std::string;
+using std::cin;
+using std::cout;
 
-struct Digit_pat_a1038 {
-	char digit[9];
-};
-
-bool cmp_pat_a1038(Digit_pat_a1038& a, Digit_pat_a1038& b) {
-	int len_a = strlen(a.digit);
-	int len_b = strlen(b.digit);
-	printf("( %d %d )", len_a, len_b);
-	int i;
-	for (i = 0; i < len_a && i < len_b; ++i) {
-		if (a.digit[i] < b.digit[i]) {
-			return true;
-		}
-	}
-	// 前面数字都相同,长的数字排在前面
-	if (i == len_a) {
-		return false;
-	}
-	else {
+bool cmp_pat_a1038(string& a, string& b) {
+	if (a + b < b + a) {
 		return true;
 	}
+	else {
+		return false;
+	}
 }
-Digit_pat_a1038 segments[10000];
+
 void pat_a1038() {
 	// 如果某个segment是全0, 则不能第一个输出
 	int N;
+	string ans;
 	// char segments[10000][9];
+	string segments[10000];
 	scanf("%d", &N);
 	for (int i = 0; i < N; ++i) {
-		scanf("%s", segments[i].digit);
+		cin >> segments[i];
 	}
-	cmp_pat_a1038(segments[0], segments[1]);
 	// 排序
 	// 因为所有数字合成一个数字后长度一样，因此每次从每个segment中选择开头最小的，
 	// 相同选择第二位最小的，继续相同选择第三位最小的。。。当前面一样时，选择长度更长的
-	/*sort(segments, segments + N, cmp_pat_a1038);
+	sort(segments, segments + N, cmp_pat_a1038);
+	// 将排序后的所有数字拼接
 	for (int i = 0; i < N; ++i) {
-		printf("%s", segments[i].digit);
-	}*/
+		ans += segments[i];
+	}
+	// 去除前导0
+	while (ans.size() != 0 && ans[0] == '0') {
+		ans.erase(ans.begin());
+	}
+	if (ans.size() > 0) {
+		cout << ans;
+	}
+	else {
+		cout << 0;
+	}
 }
 
 
-int main() {
-	pat_a1038();
-
-	return 0;
-}
+//int main() {
+//	pat_a1038();
+//
+//	return 0;
+//}
