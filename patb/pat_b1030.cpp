804d2a023a69a12edf659344e67318c8f40428be
flzq <freedomcoder@163.com>
2020-06-17 23:18:27 +0800

    pat_b1030

--- patb/pat_b1030.cpp
+++ patb/pat_b1030.cpp
@@ -3,15 +3,17 @@
 using std::sort;
 
 void pat_b1030() {
-	int N, p, max{ -1 };
-	int numbers[100000];
+	// 注意: 测试点5如果使用int,会得到错误结果(因为p<=1e9),所以要使用long long
+	long N, p;
+	int max{ -1 };
+	long numbers[100000];
 	scanf("%d%d", &N, &p);
 	for (int i = 0; i < N; ++i) {
-		scanf("%d", &numbers[i]);
+		scanf("%lld", &numbers[i]);
 	}
 	sort(numbers, numbers + N);
 	for (int i = 0; i < N; ++i) {
-		int mP = numbers[i] * p;
+		long long mP = numbers[i] * p;
 		// 找到一个数M，使得它是最后一个满足M<=mp
 		int left = i, right = N, mid;
 		while (left < right) {
