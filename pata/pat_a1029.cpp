9164781cbf27d5a903ba502950f17e8566b5bc83
flzq <freedomcoder@163.com>
2020-06-25 17:53:38 +0800

    pat_a1029

--- pata/pat_a1029.cpp
+++ pata/pat_a1029.cpp
@@ -58,19 +58,20 @@ using std::sort;
 //	}
 //}
 
-long numbers[400000];
+long long numbers[400010];
 void pat_a1029() {
 	int N1, N2;
 	scanf("%d", &N1);
 	for (int i = 0; i < N1; ++i) {
-		scanf("%ld", &numbers[i]);
+		scanf("%lld", &numbers[i]);
 	}
+	scanf("%d", &N2);
 	for (int i = 0; i < N2; ++i) {
-		scanf("%ld", &numbers[N1 + i]);
+		scanf("%lld", &numbers[N1 + i]);
 	}
 	sort(numbers, numbers + N1 + N2);
 	int mid = (N1 + N2 - 1) / 2;
-	printf("%ld", numbers[mid]);
+	printf("%lld", numbers[mid]);
 }
 
 //int main() {
