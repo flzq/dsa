bfefbd9dc551fabc856602ebd6fca259f59f5bcc
flzq <freedomcoder@163.com>
2020-07-02 13:43:54 +0800

    pat_a1056

---
+++ pata/pat_a1056.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+void pat_a1056() {
+	int Np, Ng;
+	int weights[1010];
+	int id[1010];
+	scanf("%d%d", &Np, &Ng);
+	for (int i = 0; i < Np; ++i) {
+		scanf("%d", &weights[i]);
+	}
+	for (int i = 0; i < Np; ++i) {
+		scanf("%d", &id[i]);
+	}
+
+}
+
+//int main() {
+//	pat_a1056();
+//	return 0;
+//}
