1ee23a684d0cd694a0a804895caf80b8a9850b68
flzq <freedomcoder@163.com>
2020-07-22 23:47:12 +0800

    pat_a1045(思路正确，测试点1错误)

--- pata/pat_a1045.cpp
+++ pata/pat_a1045.cpp
@@ -1,25 +1,30 @@
 #include <cstdio>
+#include <algorithm>
+using namespace std;
 
 class Pat_a1045 {
-	int N, M, L, favorite[200], stripe[10000], dp[10000];
+	int N, M, L, num{ 0 }, favorite[200], stripe[10000], dp[10000], hash_table[210];
 public:
 	void pat_a1045() {
+		int tmp;
+		fill(hash_table, hash_table + 210, -1);
 		scanf("%d%d", &N, &M);
 		for (int i = 0; i < M; ++i) {
 			scanf("%d", &favorite[i]);
+			hash_table[favorite[i]] = i;
 		}
 		scanf("%d", &L);
 		for (int i = 0; i < L; ++i) {
-			scanf("%d", &stripe[i]);
+			scanf("%d", &tmp);
+			if (hash_table[tmp] >= 0) {
+				stripe[num++] = hash_table[tmp];
+			}
 		}
 		int max_len = -1;
-		for (int i = 0; i < L; ++i) {
-			dp[i] = find_origin(i);
-			if (dp[i] == 0) {
-				continue;
-			}
+		for (int i = 0; i < num; ++i) {
+			dp[i] = 1;
 			for (int j = 0; j < i; ++j) {
-				if (dp[j]>=1 && get_index(j)<=get_index(i) && dp[j] + dp[i] > dp[i]) {
+				if (stripe[j]<=stripe[i] && dp[j] + 1 > dp[i]) {
 					dp[i] = 1 + dp[j];
 					if (max_len < dp[i]) {
 						max_len = dp[i];
@@ -28,22 +33,6 @@ public:
 			}
 		}
 		printf("%d", max_len);
-	}
-	int get_index(int index) {
-		for (int i = 0; i < M; ++i) {
-			if (stripe[index] == favorite[i]) {
-				return i;
-			}
-		}
-	}
-	int find_origin(int index) {
-		// 找到最初dp中是包含该位置的颜色
-		for (int i = 0; i < M; ++i) {
-			if (stripe[index] == favorite[i]) {
-				return 1;
-			}
-		}
-		return 0;
 	}
 };
 
