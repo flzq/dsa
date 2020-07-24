ec8878de524ffd45a661d1e5df3913e6dcf48d7e
flzq <freedomcoder@163.com>
2020-07-24 13:38:25 +0800

    pat_a1045(最长公共子序列解法都通过；最长不下降子序列解法测试点1未通过；)

--- pata/pat_a1045.cpp
+++ pata/pat_a1045.cpp
@@ -1,9 +1,11 @@
 #include <cstdio>
+#include <cstring>
 #include <algorithm>
 using namespace std;
 
-class Pat_a1045 {
-	int N, M, L, num{ 0 }, favorite[200], stripe[10000], dp[10000], hash_table[210];
+class Pat_a1045_LIS {
+	// 最长不下降子序列解法
+	int N, M, L, num{ 0 }, favorite[210], stripe[10010], dp[10010], hash_table[210];
 public:
 	void pat_a1045() {
 		int tmp;
@@ -36,8 +38,45 @@ public:
 	}
 };
 
+int dp_LCS[210][10010]{ 0 };
+class Pat_a1045_LCS {
+	// 最长公共子序列解法
+	int N, M, L, favorite[210], stripe[10010];
+public:
+	void pat_a1045() {
+		scanf("%d%d", &N, &M);
+		for (int i = 1; i <= M; ++i) {
+			scanf("%d", &favorite[i]);
+		}
+		scanf("%d", &L);
+		for (int i = 1; i <= L; ++i) {
+			scanf("%d", &stripe[i]);
+		}
+		for (int i = 0; i <= M; ++i) {
+			dp_LCS[i][0] = 0;
+		}
+		for (int i = 0; i <= L; ++i) {
+			dp_LCS[0][i] = 0;
+		}
+		for (int i = 1; i <= M; ++i) {
+			for (int j = 1; j <= L; ++j) {
+				if (favorite[i] == stripe[j]) {
+					dp_LCS[i][j] = max(dp_LCS[i - 1][j], dp_LCS[i][j - 1]) + 1;
+				}
+				else {
+					dp_LCS[i][j] = max(dp_LCS[i - 1][j], dp_LCS[i][j - 1]);
+				}
+			}
+		}
+		printf("%d", dp_LCS[M][L]);
+	}
+};
+
+
 //int main() {
-//	Pat_a1045 pat_a1045;
-//	pat_a1045.pat_a1045();
+//	//Pat_a1045_LIS pat_a1045_lis;
+//	//pat_a1045_lis.pat_a1045();
+//	Pat_a1045_LCS pat_a1045_lcs;
+//	pat_a1045_lcs.pat_a1045();
 //	return 0;
 //}
