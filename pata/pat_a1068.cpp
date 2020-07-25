db67b95c5dbbf3b73e24beae798c00ba1cbea7c6
flzq <freedomcoder@163.com>
2020-07-25 19:59:28 +0800

    pat_a1068(未完成)

--- pata/pat_a1068.cpp
+++ pata/pat_a1068.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class Pat_a1068 {
-	int N, M, coins[10010]{ 0 };
+	int N, M, coins[10010]{ 0 }, dp[10010][100]{ 0 };
 public:
 	void pat_a1068() {
 		scanf("%d%d", &N, &M);
@@ -11,7 +11,11 @@ public:
 			scanf("%d", &coins[i]);
 		}
 		sort(coins, coins + N + 1);
-
+		for (int i = 1; i <= N; ++i) {
+			for (int v = coins[i]; v <= M; ++v) {
+				dp[i][v] = max(dp[i - 1][v], dp[i][v - coins[i]] + coins[i]);
+			}
+		}
 	}
 };
 
